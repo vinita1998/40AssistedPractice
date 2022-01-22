ec1afd44fcb84576b0bd1b71b10599a342cb26ef
Vinita <Vinita@LAPTOP-T619IKD9>
2022-01-23 01:57:19 +0530

    first commit

---
+++ FileHandlingMechanism/src/CreateFile.java
@@ -0,0 +1,21 @@
+import java.io.File;
+ 
+// Import the IOException class to handle errors
+import java.io.IOException; 
+ 
+public class CreateFile {
+public static void main(String[] args) {
+try {
+// Creating an object of a file
+File myObj = new File("D:FileHandlingNewFilef1.txt"); 
+if (myObj.createNewFile()) {
+System.out.println("File created: " + myObj.getName());
+} else {
+System.out.println("File already exists.");
+}
+} catch (IOException e) {
+System.out.println("An error occurred.");
+e.printStackTrace();
+}
+}
+}
